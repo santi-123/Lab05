9fbe6b1276695660fa27e8cb7886d7073ae1f290
santi-123 <dafeveco2004@gmail.com>
2024-04-19 11:22:01 -0500

    Square 19/04 11:21

--- src/presentation/SquareGUI.java
+++ src/presentation/SquareGUI.java
@@ -1,14 +1,41 @@
 package presentation;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class SquareGUI extends JFrame {
 
   public SquareGUI() {
+    prepareElements();
+    prepareExitListener();
+  }
+
+  private void prepareElements() {
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    Dimension screenSize = toolkit.getScreenSize();
+
     setTitle("Square");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(300, 300);
+    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    setSize((screenSize.width) / 2, (screenSize.height) / 2);
     setLocationRelativeTo(null);
+  }
+
+  private void prepareExitListener() {
+    this.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        int confirm = JOptionPane.showConfirmDialog(
+            SquareGUI.this,
+            "Are you sure you want to close this window?",
+            "Exit Confirmation",
+            JOptionPane.YES_NO_OPTION);
+        if (confirm == JOptionPane.YES_OPTION) {
+          System.exit(0);
+        }
+      }
+    });
   }
 
   public static void main(String[] args) {
