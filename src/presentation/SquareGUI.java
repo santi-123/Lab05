dbf9aa3a48ee28658398aa96579a5112d71e622e
santi-123 <dafeveco2004@gmail.com>
2024-04-19 12:31:11 -0500

    19/04 12:30

--- src/presentation/SquareGUI.java
+++ src/presentation/SquareGUI.java
@@ -2,14 +2,14 @@ package presentation;
 
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 public class SquareGUI extends JFrame {
 
   public SquareGUI() {
     prepareElements();
-    prepareExitListener();
+    prepareMenu();
+    prepareActions();
   }
 
   private void prepareElements() {
@@ -22,20 +22,56 @@ public class SquareGUI extends JFrame {
     setLocationRelativeTo(null);
   }
 
-  private void prepareExitListener() {
+  private void prepareActions() {
     this.addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
         int confirm = JOptionPane.showConfirmDialog(
-            SquareGUI.this,
-            "Are you sure you want to close this window?",
-            "Exit Confirmation",
-            JOptionPane.YES_NO_OPTION);
+            SquareGUI.this, "¿Estas seguro de que deseas salir?", "Confirmacion de salida", JOptionPane.YES_NO_OPTION);
         if (confirm == JOptionPane.YES_OPTION) {
           System.exit(0);
         }
       }
     });
+  }
+
+  private void prepareMenu() {
+    JMenuBar menuBar = new JMenuBar();
+
+    JMenu menu = new JMenu("File");
+
+    JMenuItem nuevo = new JMenuItem("Nuevo");
+    JMenuItem abrir = new JMenuItem("Abrir");
+    JMenuItem guardar = new JMenuItem("Salvar");
+    JMenuItem salirM = new JMenuItem("Salir");
+
+    ActionListener mensajeCons = new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(SquareGUI.this, "En construccion");
+      }
+    };
+    nuevo.addActionListener(mensajeCons);
+    abrir.addActionListener(mensajeCons);
+    guardar.addActionListener(mensajeCons);
+    salirM.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        int confirm = JOptionPane.showConfirmDialog(
+            SquareGUI.this, "¿Estas seguro que deseas salir?", "Confirmacion de salida", JOptionPane.YES_NO_OPTION);
+        if (confirm == JOptionPane.YES_OPTION) {
+          System.exit(0);
+        }
+      }
+    });
+    menu.add(nuevo);
+    menu.addSeparator();
+    menu.add(abrir);
+    menu.addSeparator();
+    menu.add(guardar);
+    menu.addSeparator();
+    menu.add(salirM);
+    menuBar.add(menu);
+
+    setJMenuBar(menuBar);
   }
 
   public static void main(String[] args) {
